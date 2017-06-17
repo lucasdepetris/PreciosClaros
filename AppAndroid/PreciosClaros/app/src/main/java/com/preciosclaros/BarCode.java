6dbd1837fda1a35d8f0460c2a8ea223db940e92d
lucasdepetris <lucasdepetris1@outlook.com>
2017-06-17 16:09:06 -0300

    android coordenadas

--- AppAndroid/PreciosClaros/app/src/main/java/com/preciosclaros/BarCode.java
+++ AppAndroid/PreciosClaros/app/src/main/java/com/preciosclaros/BarCode.java
@@ -5,15 +5,19 @@ package com.preciosclaros;
  */
 
 import android.*;
+import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
+import android.location.Address;
 import android.location.Criteria;
+import android.location.Geocoder;
 import android.location.GpsStatus;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.location.LocationProvider;
 import android.os.Build;
 import android.os.Bundle;
 import android.provider.Settings;
@@ -35,8 +39,10 @@ import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
 import com.squareup.picasso.Picasso;
 
+import java.io.IOException;
 import java.text.NumberFormat;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
 import java.util.concurrent.TimeUnit;
 
@@ -63,12 +69,21 @@ public class BarCode extends AppCompatActivity implements View.OnClickListener {
     public Call<com.preciosclaros.Response> requestCatalog;
     public Context context = this;
     public Double latitud ,longitud;
-    private LocationManager locationManager;
-    private LocationListener listener;
+    public SharedPreferences sharedPreferences;
+    SharedPreferences.Editor editor;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.producto_por_codigo);
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION,}, 1000);
+        } else {
+            locationStart();
+        }
+       /* sharedPreferences = getApplicationContext().getSharedPreferences("Reg", 0);
+        if (sharedPreferences.contains("Latitude")){
+            Toast.makeText(this,"latitud:"+Double.parseDouble(sharedPreferences.getString("Lat",""))+"Longitude: "+Double.parseDouble(sharedPreferences.getString("Longitude","")),Toast.LENGTH_LONG).show();
+        }else{ Toast.makeText(this,"latitud: NO HAY",Toast.LENGTH_LONG).show();}*/
 
         HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
         interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
@@ -116,37 +131,15 @@ public class BarCode extends AppCompatActivity implements View.OnClickListener {
 
     public void buscarProducto(final String codigo) {
         //OBTENER UBICACION
-        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
-        listener = new LocationListener() {
-            @Override
-            public void onLocationChanged(Location location) {
-                latitud = location.getLatitude();
-                longitud = location.getLongitude();
-                String coordenadas = "Mis coordenadas son: " + "Latitud = " + location.getLatitude() +"Longitud = "+ location.getLongitude()+"";
-                Toast.makeText(getApplicationContext(),coordenadas,Toast.LENGTH_LONG).show();
-            }
-
-            @Override
-            public void onStatusChanged(String s, int i, Bundle bundle) {
-
-            }
-
-            @Override
-            public void onProviderEnabled(String s) {
-
-            }
-
-            @Override
-            public void onProviderDisabled(String s) {
-
-                Intent i = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
-                startActivity(i);
-            }
-        };
-        configure_button();
+        double lati ,lng;
+        sharedPreferences = getApplicationContext().getSharedPreferences("Reg", 0);
+        if(sharedPreferences.contains("Lat")){
+            lati = Double.parseDouble(sharedPreferences.getString("Lat",""));
+            lng = Double.parseDouble(sharedPreferences.getString("Longitude",""));
+        }else {lati = 0.0; lng = 0.0;}
         //OBTENER UBICACION
         double d =-34.6556262;
-        requestCatalog = service.getProducto(codigo, d, -58.589724);
+        requestCatalog = service.getProducto(codigo, lati, lng);
         requestCatalog.enqueue(new Callback<com.preciosclaros.Response>() {
             @Override
             public void onResponse(Call<com.preciosclaros.Response> call, Response<com.preciosclaros.Response> response) {
@@ -180,28 +173,112 @@ public class BarCode extends AppCompatActivity implements View.OnClickListener {
 
         });
     }
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        switch (requestCode){
-            case 10:
-                configure_button();
-                break;
-            default:
-                break;
+    private void locationStart() {
+        LocationManager mlocManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        Localizacion Local = new Localizacion();
+        Local.setMainActivity(this);
+        final boolean gpsEnabled = mlocManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+        if (!gpsEnabled) {
+            Intent settingsIntent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+            startActivity(settingsIntent);
+        }
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION,}, 1000);
+            return;
+        }
+        mlocManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, (LocationListener) Local);
+        mlocManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, (LocationListener) Local);
+
+       /* mensaje1.setText("Localizacion agregada");
+        mensaje2.setText("");*/
+    }
+
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        if (requestCode == 1000) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                locationStart();
+                return;
+            }
         }
     }
 
-    void configure_button(){
-        // first check for permissions
-        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                requestPermissions(new String[]{android.Manifest.permission.ACCESS_COARSE_LOCATION, android.Manifest.permission.ACCESS_FINE_LOCATION, android.Manifest.permission.INTERNET}
-                        ,10);
-            }
-            return;
-        }
-        locationManager.requestLocationUpdates("gps", 5000, 0, listener);
-        // this code won't execute IF permissions are not allowed, because in the line above there is return statement.
+    public void setLocation(Location loc) {
+        //Obtener la direccion de la calle a partir de la latitud y la longitud
+        if (loc.getLatitude() != 0.0 && loc.getLongitude() != 0.0) {
+            try {
+                Geocoder geocoder = new Geocoder(this, Locale.getDefault());
+                List<Address> list = geocoder.getFromLocation(
+                        loc.getLatitude(), loc.getLongitude(), 1);
+                if (!list.isEmpty()) {
+                    Address DirCalle = list.get(0);
+                   /* mensaje2.setText("Mi direccion es: \n"
+                            + DirCalle.getAddressLine(0));*/
+                }
 
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+    /* Aqui empieza la Clase Localizacion */
+    public class Localizacion implements LocationListener {
+        BarCode mainActivity;
+
+        public BarCode getMainActivity() {
+            return mainActivity;
+        }
+
+        public void setMainActivity(BarCode mainActivity) {
+            this.mainActivity = mainActivity;
+        }
+
+        @Override
+        public void onLocationChanged(Location loc) {
+            // Este metodo se ejecuta cada vez que el GPS recibe nuevas coordenadas
+            // debido a la deteccion de un cambio de ubicacion
+
+            loc.getLatitude();
+            loc.getLongitude();
+            sharedPreferences = getApplicationContext().getSharedPreferences("Reg", 0);
+            // get editor to edit in file
+            editor = sharedPreferences.edit();
+            editor.putLong("Latitude", Double.doubleToLongBits(loc.getLatitude()));
+            editor.putString("Lat", String.valueOf(loc.getLatitude()));
+            editor.putString("Longitude", String.valueOf(loc.getLongitude()));
+            editor.commit();
+            /*String Text = "Mi ubicacion actual es: " + "\n Lat = "
+                    + loc.getLatitude() + "\n Long = " + loc.getLongitude();
+            mensaje1.setText(Text);*/
+            this.mainActivity.setLocation(loc);
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+            // Este metodo se ejecuta cuando el GPS es desactivado
+            Toast.makeText(getApplicationContext(),"GPS Desactivado",Toast.LENGTH_SHORT).show();
+            // mensaje1.setText("GPS Desactivado");
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+            // Este metodo se ejecuta cuando el GPS es activado
+            Toast.makeText(getApplicationContext(),"GPS Activado",Toast.LENGTH_SHORT).show();
+            //mensaje1.setText("GPS Activado");
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+            switch (status) {
+                case LocationProvider.AVAILABLE:
+                    Log.d("debug", "LocationProvider.AVAILABLE");
+                    break;
+                case LocationProvider.OUT_OF_SERVICE:
+                    Log.d("debug", "LocationProvider.OUT_OF_SERVICE");
+                    break;
+                case LocationProvider.TEMPORARILY_UNAVAILABLE:
+                    Log.d("debug", "LocationProvider.TEMPORARILY_UNAVAILABLE");
+                    break;
+            }
+        }
     }
 }
